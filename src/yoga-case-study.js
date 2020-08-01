d72c5c00aeafff89de6cd4c82eb0d2c3562f5ea8
Peter Oberrauner <thealmostbearded@gmail.com>
2020-08-01 11:10:07 +0200

    Implement a bar chart with Yoga layouting

--- src/yoga-case-study.js
+++ src/yoga-case-study.js
@@ -1,24 +1,200 @@
-import yoga from 'yoga-layout-prebuilt';
+import * as d3 from 'd3';
+import { svg } from 'd3';
+import yogaLayoutParser from './yoga-layout-parser';
 
-const root = yoga.Node.create();
-root.setFlexDirection(yoga.FLEX_DIRECTION_COLUMN);
-root.setAlignItems(yoga.ALIGN_STRETCH);
+// Data representing the populations of large Austrian cities
+var data = [
+  { city: 'Vienna', population: 1691000 },
+  { city: 'Graz', population: 222000 },
+  { city: 'Linz', population: 205000 },
+  { city: 'Salzburg', population: 153000 },
+  { city: 'Innsbruck', population: 132000 },
+  { city: 'Klagenfurt', population: 91000 },
+  { city: 'Villach', population: 59000 },
+];
 
+// Configuration for the chart
+var config = {
+  numericAxis: {
+    visible: true,
+    title: 'Population',
+  },
+  categoricAxis: {
+    visible: true,
+    title: 'Countries',
+  },
+};
 
-const node1 = yoga.Node.create();
-node1.setHeight(100)
-node1.setFlexGrow(0);
-node1.setFlexShrink(0);
+// Calculating size of the container
+var containerSelection = d3.select('#chart');
+var containerBoundingRect = containerSelection.node().getBoundingClientRect();
+var containerSize = [containerBoundingRect.width, containerBoundingRect.height];
 
+// Setting up the initial scales for the full size of the container
+var categoricScale = d3
+  .scaleBand()
+  .domain(data.map((d) => d.city))
+  .padding(0.05)
+  .range([0, containerSize[0]]);
 
-const node2 = yoga.Node.create();
-node2.setFlexGrow(1);
+var numericScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(data, (d) => d.population)])
+  .range([containerSize[1], 0]);
 
-root.insertChild(node1, 0);
-root.insertChild(node2, 1);
+// Create the root node of the chart
+var svgSelection = containerSelection
+  .append('svg')
+  .attr('viewBox', `0, 0, ${containerSize[0]}, ${containerSize[1]}`);
 
+// Cached selection variables
+var numericAxisSelection, categoricAxisSelection, barsSelection;
 
-root.calculateLayout(500, 300);
-console.log(root.getComputedLayout());
-console.log(node1.getComputedLayout());
-console.log(node2.getComputedLayout());
+// Create an initial scaffold of the chart that reflects the desired layout
+scaffoldChart();
+
+// Initial rendering of the axes to access their sizes during layouting
+renderCategoricAxis();
+renderNumericAxis();
+
+// Parse the node hierarchy and calculate the layout
+var layoutParser = yogaLayoutParser();
+layoutParser.parseNodeHierarchy(svgSelection.node());
+layoutParser.calculateLayout(containerSize[0], containerSize[1]);
+
+// Resize the range of the scale to fit into the calculated size of the bar drawing area
+var barsLayoutNode = layoutParser.nodeToLayoutNodeMap.get(barsSelection.node());
+categoricScale.range([0, barsLayoutNode.getComputedWidth()]);
+numericScale.range([barsLayoutNode.getComputedHeight(), 0]);
+
+// Rerender the axes and render the bars now that the scales have correct ranges
+renderCategoricAxis();
+renderNumericAxis();
+renderBars();
+
+// Position the different nodes according to the layout
+layoutParser.applyLayout();
+
+// Scaffold the desired layout of the chart using yogaLayout attributes
+function scaffoldChart() {
+  // This function has been organized using unnecessary blocks to reflect nesting of nodes
+
+  svgSelection.attr(
+    'yogaLayout',
+    'flex-direction: column; align-items: stretch; padding-left: 25; padding-right: 25'
+  );
+  {
+    var row1Selection = svgSelection
+      .append('g')
+      .attr(
+        'yogaLayout',
+        'flex-direction: row;align-items: stretch;flex-grow: 1'
+      );
+    {
+      numericAxisSelection = row1Selection
+        .append('g')
+        .classed('numeric-axis', true)
+        .attr(
+          'yogaLayout',
+          'flex-direction: row; flex-grow: 0; flex-shrink: 0'
+        );
+      {
+        numericAxisSelection
+          .append('text')
+          .classed('title', true)
+          .attr(
+            'yogaLayout',
+            'width: auto; align-self: center; flex-grow: 0; flex-shrink: 0; margin-right: 10'
+          );
+
+        numericAxisSelection
+          .append('g')
+          .classed('ticks', true)
+          .attr('yogaLayout', 'width: auto; flex-grow: 0; flex-shrink: 0');
+      }
+
+      barsSelection = row1Selection
+        .append('g')
+        .classed('bars', true)
+        .attr('yogaLayout', 'flex-grow: 1');
+    }
+
+    categoricAxisSelection = svgSelection
+      .append('g')
+      .classed('categoric-axis', true)
+      .attr(
+        'yogaLayout',
+        'flex-direction: column; margin-left: $.numeric-axis#width; flex-grow: 0; flex-shrink: 0'
+      );
+    {
+      categoricAxisSelection
+        .append('g')
+        .classed('ticks', true)
+        .attr('yogaLayout', 'height: auto');
+
+      categoricAxisSelection
+        .append('text')
+        .classed('title', true)
+        .attr('yogaLayout', 'height: auto; align-self: center; margin-top: 10');
+    }
+  }
+}
+
+function renderCategoricAxis() {
+  categoricAxisSelection.attr(
+    'transform',
+    `scale(${+config.categoricAxis.visible}, 1)`
+  );
+
+  categoricAxisSelection
+    .select('.ticks')
+    .call(d3.axisBottom(categoricScale))
+    .attr('font-size', null)
+    .attr('font-family', null);
+
+  categoricAxisSelection
+    .select('.title')
+    .attr('text-anchor', 'middle')
+    .attr('dy', '0.7em')
+    .attr('font-weight', 'bold')
+    .text(config.categoricAxis.title || '');
+}
+
+function renderNumericAxis() {
+  numericAxisSelection.attr(
+    'transform',
+    `scale(${+config.numericAxis.visible}, 1)`
+  );
+
+  numericAxisSelection
+    .select('.title')
+    .attr('text-anchor', 'middle')
+    .attr('dy', '0.7em')
+    .attr('font-weight', 'bold')
+    .attr('transform', 'rotate(-90)')
+    .text(config.numericAxis.title || '');
+
+  numericAxisSelection
+    .select('.ticks')
+    .call(d3.axisLeft(numericScale))
+    .attr('font-size', null)
+    .attr('font-family', null)
+    .call((e) =>
+      e.attr(
+        'transform',
+        `translate(${e.node().getBoundingClientRect().width}, 0)`
+      )
+    );
+}
+
+function renderBars() {
+  barsSelection
+    .attr('fill', 'steelblue')
+    .selectAll('rect')
+    .data(data)
+    .join('rect')
+    .attr('x', (d) => categoricScale(d.city))
+    .attr('y', (d) => numericScale(d.population))
+    .attr('height', (d) => numericScale(0) - numericScale(d.population))
+    .attr('width', categoricScale.bandwidth());
+}
---
+++ src/yoga-layout-parser.js
@@ -0,0 +1,204 @@
+import yoga from 'yoga-layout-prebuilt';
+
+export default function yogaLayoutParser() {
+  // The root DOM node of the parsed DOM hierarchy
+  var rootNode = null;
+
+  // Maps DOM nodes to Yoga nodes
+  var nodeToLayoutNodeMap = new Map();
+
+  // Whether or not the layout has already been calculated
+  var layoutCalculated = false;
+
+  // Property parser that can only be applied after the layout has been calculated
+  // This is very useful for things like 2D positioning because Flexbox lacks this
+  // possibility out of the box. If a yogaLayout property has a value of the form of
+  // $some-query-selector#some-rect-property, the property needs access to the already
+  // layed out dimensions of the node. Therefore, the parsing of this property is
+  // delayed until after the layout has been calculated, which forces a recalculation
+  // after the application of the post-layout properties.
+  var postLayoutPropertyParsers = [];
+
+  // This map defines how to parse the different yogaLayout properties
+  // WARNING: Not all properties are parsed yet.
+  var propParseMap = {
+    'flex-direction': {
+      fnc: 'setFlexDirection',
+      params: {
+        column: yoga.FLEX_DIRECTION_COLUMN,
+        row: yoga.FLEX_DIRECTION_ROW,
+      },
+    },
+    'align-items': {
+      fnc: 'setAlignItems',
+      params: {
+        stretch: yoga.ALIGN_STRETCH,
+        center: yoga.ALIGN_CENTER,
+      },
+    },
+    'align-self': {
+      fnc: 'setAlignSelf',
+      params: {
+        stretch: yoga.ALIGN_STRETCH,
+        center: yoga.ALIGN_CENTER,
+      },
+    },
+    'flex-grow': {
+      fnc: 'setFlexGrow',
+      paramsFnc: parseLayoutPropertyValue,
+    },
+    'flex-shrink': {
+      fnc: 'setFlexShrink',
+      paramsFnc: parseLayoutPropertyValue,
+    },
+    width: {
+      fnc: 'setWidth',
+      paramsFnc: parseLayoutPropertyValue,
+    },
+    height: {
+      fnc: 'setHeight',
+      paramsFnc: parseLayoutPropertyValue,
+    },
+  };
+
+  // The following code automatically generates parse mappings for edge-based
+  // properties like e.g. margin-top/right/bottom/left etc.
+  var edgeBasedProps = ['margin', 'padding'];
+  var edgeBasedPropFncs = ['setMargin', 'setPadding'];
+  var edgePropStrings = ['top', 'right', 'bottom', 'left'];
+  var edgesPropValues = [
+    yoga.EDGE_TOP,
+    yoga.EDGE_RIGHT,
+    yoga.EDGE_BOTTOM,
+    yoga.EDGE_LEFT,
+  ];
+  for (var propIndex = 0; propIndex < edgeBasedProps.length; ++propIndex) {
+    for (var edgeIndex = 0; edgeIndex < edgePropStrings.length; ++edgeIndex) {
+      propParseMap[
+        `${edgeBasedProps[propIndex]}-${edgePropStrings[edgeIndex]}`
+      ] = {
+        fnc: edgeBasedPropFncs[propIndex],
+        paramsFnc: ((edgeValue) => (node, key, value) => [
+          edgeValue,
+          ...parseLayoutPropertyValue(node, key, value),
+        ])(edgesPropValues[edgeIndex]),
+      };
+    }
+  }
+
+  // Parse a yogaLayout property
+  function parseLayoutProperty(node, layoutNode, key, value) {
+    var parsedProp = propParseMap[key];
+
+    if (parsedProp) {
+      if (parsedProp.params) {
+        // Property parameters can be parsed via a simple mapping
+
+        // console.log(`${parsedProp.fnc}(${parsedProp.params[value]})`);
+        layoutNode[parsedProp.fnc](parsedProp.params[value]);
+      } else if (parsedProp.paramsFnc) {
+        // Property parameters are parsed via a parse function
+
+        if (value.indexOf('$') < 0 || layoutCalculated) {
+          // Property is either no post-layout property or the layout has already been calculated
+
+          // console.log(
+          //   `${parsedProp.fnc}(${parsedProp.paramsFnc(node, key, value)})`
+          // );
+          layoutNode[parsedProp.fnc](...parsedProp.paramsFnc(node, key, value));
+        } else {
+          // Delay parsing of property until after layout calculation
+          postLayoutPropertyParsers.push(function () {
+            parseLayoutProperty(node, layoutNode, key, value);
+          });
+        }
+      }
+    } else {
+      console.warn(`Unparsable layout property: (${key}: ${value})`);
+    }
+  }
+
+  // Parses the value of a yogaLayout property
+  function parseLayoutPropertyValue(node, key, value) {
+    return [
+      value[0] === '$'
+        ? parseLayoutPropertyVariable(value)
+        : value === 'auto'
+        ? node.getBoundingClientRect()[key]
+        : value,
+    ];
+  }
+
+  // Parses the value variable of a yogaLayout property
+  function parseLayoutPropertyVariable(value) {
+    var splitStr = value.substr(1).split('#');
+    var node = document.querySelector(splitStr[0]);
+    return nodeToLayoutNodeMap.get(node).getComputedLayout()[splitStr[1]];
+  }
+
+  return {
+    rootNode,
+    nodeToLayoutNodeMap,
+    parseNodeHierarchy(node) {
+      // console.log(node);
+      rootNode = rootNode || node;
+
+      var layoutNode = yoga.Node.create();
+      nodeToLayoutNodeMap.set(node, layoutNode);
+
+      // Clean up the yogaLayout properties string
+      var propsString = node.getAttribute('yogaLayout');
+      var props = propsString
+        .split(';')
+        .map((prop) => prop.split(':').map((str) => str.trim()));
+
+      // Parse all yogaLayout properties
+      for (var i = 0; i < props.length; ++i) {
+        parseLayoutProperty(node, layoutNode, props[i][0], props[i][1]);
+      }
+
+      // Parse children recursively
+      for (var i = 0; i < node.children.length; ++i) {
+        var childNode = node.children[i];
+
+        // Only parse children with yogaLayout attributes
+        // For this parser to work correctly you have to make sure that a level of
+        // the DOM hierarchy either contains yogaLayout attributes on all nodes or
+        // on none.
+        if (childNode.getAttribute('yogaLayout')) {
+          var childLayoutNode = this.parseNodeHierarchy(childNode);
+
+          // Child index might not be correct if non yogaLayout nodes are mixed with yogaLayout
+          // nodes on the same level of the DOM hierarchy.
+          layoutNode.insertChild(childLayoutNode, i);
+        }
+      }
+      return layoutNode;
+    },
+    calculateLayout(width, height) {
+      // Calculate the layout
+      var rootLayoutNode = nodeToLayoutNodeMap.get(rootNode);
+      rootLayoutNode.calculateLayout(width, height);
+      layoutCalculated = true;
+
+      // Apply post-layout property parsers
+      for (var i = 0; i < postLayoutPropertyParsers.length; ++i) {
+        postLayoutPropertyParsers[i]();
+      }
+
+      // Recalculate the layout
+      rootLayoutNode.calculateLayout(width, height);
+    },
+    applyLayout() {
+      // Apply a translation on all yogaLayout nodes to mirror the layout.
+      for (var [node, layoutNode] of nodeToLayoutNodeMap.entries()) {
+        node.setAttribute(
+          'transform',
+          `translate(${layoutNode.getComputedLeft()}, ${layoutNode.getComputedTop()})${
+            node.getAttribute('transform') || ''
+          }`
+        );
+      }
+    },
+  };
+}
